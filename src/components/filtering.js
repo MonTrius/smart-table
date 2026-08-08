88c3fbed67317429b841be843efb2dbeb82ff5f5
Baclanov Nikolay <baclanov.nik12354@yandex.ru>
2026-08-09 01:00:25 +0300

    filtering fix

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -5,18 +5,16 @@ const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName]).map(name => {                        // используйте name как значение и текстовое содержимое
-                                                        // @todo: создать и вернуть тег опции
+    Object.keys(indexes).forEach((elementName) => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName]).map(name => {
                 const option = document.createElement('option');
                 option.value = name;
                 option.textContent = name;
                 return option;
             })
         );
-     });
+    });
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
